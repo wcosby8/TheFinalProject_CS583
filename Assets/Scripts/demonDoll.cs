bd74d7a58a1ac78ac026ed5f633034dab1b4a288
LuluVnB <paraguya.gian@gmail.com>
2025-12-02 00:48:52 -0800

    fetched player position

---
+++ Assets/Scripts/demonDoll.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class demonDoll : MonoBehaviour{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Transform playerTransform;
+    void Start(){
+        GameObject player = GameObject.Find("Player");
+        playerTransform = player.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
